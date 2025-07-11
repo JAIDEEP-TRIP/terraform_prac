2b4d42c25d899b417d09e70592bcddda534f3806
jaideep <jaideeptripurani12@gmail.com>
2025-07-11 12:57:23 -0500

    day3

---
+++ day-3-statefile/varibles.tf
@@ -0,0 +1,11 @@
+variable "ami_id" {
+    description = "value of the AMI ID to use for the instance"
+    type        = string
+    
+}
+
+variable "instance_type" {
+    description = "Type of instance to use"
+    type        = string
+    
+}
