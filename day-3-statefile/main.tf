27c27a2e0ffd0c6451b5bb8f5277853b4c8e31c9
jaideep <jaideeptripurani12@gmail.com>
2025-07-11 13:33:32 -0500

    jaideep

--- day-3-statefile/main.tf
+++ day-3-statefile/main.tf
@@ -1,12 +1,10 @@
+
 resource "aws_instance" "name" {
-    ami = var.ami_id
-    instance_type = var.instance_type
-
-  
-}
-
-
-resource "aws_vpc" "name" {
-    cidr_block = "10.0.0.0/16"
+    ami = "ami-05ffe3c48a9991133"
+    instance_type = "t2.micro"
+    tags = {
+      Name = "state"
+    }
+    
   
 }
