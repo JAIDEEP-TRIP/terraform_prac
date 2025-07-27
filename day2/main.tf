637433428c8c78f50721fe6506eac5660ab1dcff
JAIDEEP-TRIP <164716355+JAIDEEP-TRIP@users.noreply.github.com>
2025-07-26 22:31:51 -0500

    Update main.tf

--- day2/main.tf
+++ day2/main.tf
@@ -3,21 +3,4 @@ resource "aws_instance" "name" {
     instance_type = var.instance_type
 }
 
-resource "aws_instance" "newserver" {
-    ami           = var.ami_id
-    instance_type = var.instance_type
 
-    tags = {
-      name = "newserver"
-    }
-}
-
-resource "aws_instance" "nanoserver"{
-    ami = var.ami_id
-    instance_type = var.instance_type
-
-    tags = {
-        Name = "nanoserver"
-    }
-
-}
