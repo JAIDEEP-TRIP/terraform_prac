7287e912e8bf04447005dfde3c0811b08449ca8e
jaideep <jaideeptripurani12@gmail.com>
2025-07-26 22:34:18 -0500

    jaideep

--- day1/main.tf
+++ day1/main.tf
@@ -1,10 +1,7 @@
-resource "aws_s3_bucket" "name" {
-    bucket = "terraform-bucket122438urnfjdv98rg"
-  
-}
+
 
 resource "aws_instance" "name" {
     ami = "ami-05ffe3c48a9991133"
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
   
 }
